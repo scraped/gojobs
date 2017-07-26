9e6bd174635a2c4147ee3780b7546316e091a220
Andrey Kazakov <tosk-nn@yandex.ru>
2017-07-26 22:55:53 +0300

    Work on project..

--- index.js
+++ index.js
@@ -1,83 +1,71 @@
 const express = require("express");
 const hbars = require("express-handlebars").create({
-  defaultLayout: "index",
+  defaultLayout: "main",
   extname: ".hbs"
 });
-
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://andrew:qwerty@ds157521.mlab.com:57521/goj-jobs", {
+  useMongoClient: true,
+  /* other options */
+});
+const Jobs = require("./models/jobs.js");
 
 app.engine(".hbs", hbars.engine);
 
 app.set("view engine", ".hbs");
 app.set("port", process.env.PORT || 3000);
 
-// FILES
+// BD
 
-let jobs;
-// let jobs = require("./response-example.json");
-const request = require("request");
-
-let options1 = {
-  url: "https://ru.socialclub.rockstargames.com/",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
+Jobs.find((err, jobs) => {
+  if (err) {
+    return console.error(err);
   }
-};
 
-let options2 = {
-  url: "https://ru.socialclub.rockstargames.com/games/gtav/pc/jobs/",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
+  if (jobs.length) {
+    return;
   }
-}
 
-let options3 = {
-  url: "https://socialclub.rockstargames.com/games/gtav/ajax/search",
-  method: "POST",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-  },
-  formData: {
-    "onlyCount": "false",
-    "offset": 0,
-    "SearchOptSubType": "",
-    "SearchOptPublisher": "members",
-    "SearchOptDate":"today",
-    "SearchOptSort":"Liked",
-    "SearchOptPlayers":"",
-    "SearchText":""
-  }
-}
+  new Jobs({
+    name: "Работа 1",
+    desc: "Описание работы 1",
+    author: "andreww",
+    rating: 80
+  }).save();
 
-request(options1, (err, res, body) => {
-  let cookies = res.headers["set-cookie"];
-  cookies["RSWSID"] = cookies[1].match(/=([^;]*)/)[1]; // RockStarWebSessionId
-  cookies["CSRFToken"] = cookies[2].match(/=([^;]*)/)[1]; // CSRFToken
-  cookies["prod"] = cookies[3].match(/=([^;]*)/)[1]; // prod
+  new Jobs({
+    name: "Работа 2",
+    desc: "Описание работы 2",
+    author: "andreww",
+    rating: 60
+  }).save();
 
-  options2.headers.Cookie = "UAGD=1/1/1990; UAGC=1; gtav_jobsview=cols; CSRFToken=" + cookies["CSRFToken"] + "; prod=" + cookies["prod"] + "; RockStarWebSessionId=" + cookies["RSWSID"] + ";";
+  new Jobs({
+    name: "Работа 3",
+    desc: "Описание работы 3",
+    author: "wefgwgf",
+    rating: 30
+  }).save();
 
-  request(options2, (err2, res2, body2) => {
-    let token = body2.match(/value="([a-zA-Z0-9_-]*)" \/>\s*<\/div/mi);
-    options3.headers.Cookie = options2.headers.Cookie;
-    options3.headers.RequestVerificationToken = token[1];
-
-    request(options3, (err3, res3, body3) => {
-      jobs = JSON.parse(body3);
-      console.log(jobs);
-    });
-  });
 });
 
+// FILES
+
+let jobs = require("./response-example.json");
+
+app.use((req, res, next) => {
+  if (!res.locals.partials) {
+    res.locals.partials = {};
+  }
+
+  res.locals.partials.jobsContext = jobs;
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    jobs: jobs.Missions
-  });
+  res.render("index");
 });
 
 // STATICS
--- test.js
+++ test.js
@@ -1,32 +1,28 @@
 const request = require("request");
+const schedule = require("node-schedule");
+const fs = require("fs");
 
-let options1 = {
-  url: "https://ru.socialclub.rockstargames.com/",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
-  }
-};
+let jobsTotal = 0;
+let jobsProcessed = 0;
 
-let options2 = {
-  url: "https://ru.socialclub.rockstargames.com/games/gtav/pc/jobs/",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
-  }
-}
+schedule.scheduleJob("0 * * * * *", () => {
+  const date = new Date();
+  const currentDate = date.getDate() + "-" + (date.getMonth() + 1) + "-"
+    + date.getFullYear() + "_" + date.getHours() + "-" + date.getMinutes()
+    + "-" + date.getSeconds();
+  const resFilename = "./response/" + currentDate + ".json";
 
-let options3 = {
-  url: "https://socialclub.rockstargames.com/games/gtav/ajax/search",
-  method: "POST",
-  headers: {
-    "Accept-Language": "ru-RU,ru",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-  },
-  formData: {
+  let options = {
+    url: "https://ru.socialclub.rockstargames.com/",
+    headers: {
+      "Accept-Language": "ru-RU,ru",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
+    }
+  };
+
+  let formData = {
     "onlyCount": "false",
-    "offset": 0,
+    "offset": jobsProcessed,
     "SearchOptSubType": "",
     "SearchOptPublisher": "members",
     "SearchOptDate":"today",
@@ -34,23 +30,56 @@ let options3 = {
     "SearchOptPlayers":"",
     "SearchText":""
   }
-}
 
-request(options1, (err, res, body) => {
-  let cookies = res.headers["set-cookie"];
-  cookies["RSWSID"] = cookies[1].match(/=([^;]*)/)[1]; // RockStarWebSessionId
-  cookies["CSRFToken"] = cookies[2].match(/=([^;]*)/)[1]; // CSRFToken
-  cookies["prod"] = cookies[3].match(/=([^;]*)/)[1]; // prod
+  console.log(new Date(), "<Start job execution>");
 
-  options2.headers.Cookie = "UAGD=1/1/1990; UAGC=1; gtav_jobsview=cols; CSRFToken=" + cookies["CSRFToken"] + "; prod=" + cookies["prod"] + "; RockStarWebSessionId=" + cookies["RSWSID"] + ";";
+  request(options, (err, res, body) => {
+    let cookies = res.headers["set-cookie"];
+    cookies["RSWSID"] = cookies[1].match(/=([^;]*)/)[1]; // RockStarWebSessionId
+    cookies["CSRFToken"] = cookies[2].match(/=([^;]*)/)[1]; // CSRFToken
+    cookies["prod"] = cookies[3].match(/=([^;]*)/)[1]; // prod
 
-  request(options2, (err2, res2, body2) => {
-    let token = body2.match(/value="([a-zA-Z0-9_-]*)" \/>\s*<\/div/mi);
-    options3.headers.Cookie = options2.headers.Cookie;
-    options3.headers.RequestVerificationToken = token[1];
+    options.headers.Cookie = "UAGD=1/1/1990; UAGC=1; gtav_jobsview=cols; CSRFToken=" + cookies["CSRFToken"] + "; prod=" + cookies["prod"] + "; RockStarWebSessionId=" + cookies["RSWSID"] + ";";
+    options.url = "https://ru.socialclub.rockstargames.com/games/gtav/pc/jobs/";
 
-    request(options3, (err3, res3, body3) => {
-      console.log(body3);
+    request(options, (err2, res2, body2) => {
+      const token = body2.match(/value="([a-zA-Z0-9_-]*)" \/>\s*<\/div/mi);
+
+      options.url = "https://socialclub.rockstargames.com/games/gtav/ajax/search";
+      options.headers.RequestVerificationToken = token[1];
+      options.headers["X-Requested-With"] = "XMLHttpRequest";
+      options.method = "POST";
+      options.formData = formData;
+
+      request(options, (err3, res3, body3) => {
+        let jobs = JSON.parse(body3);
+
+        if (!jobsTotal) {
+          jobsTotal = jobs.Total;
+        }
+
+        fs.open(resFilename, "w", 0644, (err, descriptor) => {
+          if (!err) {
+            fs.write(descriptor, body3, 0, "utf8", (err, written, str) => {
+              if (!err) {
+                jobsProcessed += jobs.Count;
+                if (jobsProcessed == jobsTotal) {
+                  this.cancel();
+                }
+              } else {
+                console.log("Can't write response file:", err);
+              }
+            });
+          } else {
+            console.error("Can't open response file:", err)
+          }
+        });
+      });
     });
   });
+
+  console.log(new Date(), "<End job execution>");
+
 });
+
+console.log(new Date(), "The job has been started");
