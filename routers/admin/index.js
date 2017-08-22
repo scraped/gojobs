870d72e3cfd95f90b9656ce5800dbcf5fa57e1cc
Andrew Kazakov <tosk-nn@yandex.ru>
2017-08-22 12:30:37 +0300

    Work on project..

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -4,24 +4,14 @@ const fetchJobs = require('../../lib/fetch-jobs');
 
 module.exports = router;
 
-router.get('/addcrew', (req, res, next) => {
-  let crewId = req.query.id;
-  console.log(typeof crewId);
-  if (crewId === '') res.send('1');
-  res.send(crewId);
-});
-
 router.get('/', (req, res) => {
   if (req.query.link === '') {
-    res.set('Content-Type', 'text/plain');
     res.send('Enter something');
   } else if (req.query.link) {
     const link = req.query.link;
     const isUser = link.match(/\s*\/member\/(\w+)\/\s*/);
     const isCrew = link.match(/\s*\?publisher=crew(\d+)\s*$/);
     const isJob = link.match(/\s*\/job\/(\w+)\s*$/);
-
-    res.set('Content-Type', 'text/plain');
 
     if (isUser) {
       fetchJobs({ member: isUser[1] });
@@ -38,3 +28,16 @@ router.get('/', (req, res) => {
     res.render('admin');
   }
 });
+
+router.get('/addcrew', (req, res, next) => {
+  if (!Number(req.query.crewid)) {
+    res.send('Incorrect crew ID.');
+  } else {
+    next();
+  }
+});
+
+router.get('/addcrew', (req, res) => {
+  const crewId = req.query.crewid;
+  res.send(`We'll trying to add this crew to the database.`);
+})
