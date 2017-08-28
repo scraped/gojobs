b77c652ed149bd387cffbbd10bc342b2ec8744d6
Andrew Kazakov <tosk-nn@yandex.ru>
2017-08-28 12:29:35 +0300

    .

--- models/job.js
+++ models/job.js
@@ -3,65 +3,75 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let jobSchema = new Schema({
-  jobID: { type: String, required: true, unique: true },
-  flags: {
-    rstar: { type: Boolean },
-    rstarverif: { type: Boolean }
-  },
+  jobId: { type: String, required: true, unique: true },
+
   name: { type: String, required: true, trim: true },
   desc: { type: String, required: true, trim: true },
-  images: {
-    rstar: { type: Number, required: true },
-    photos: { type: [String] }
-  },
   platform: { type: Number, required: true },
+  creator: { type: String, required: true },
+  images: {
+    rstar: { type: String, required: true },
+    custom: { type: [String] }
+  },
 
-  info: {
+  job: {
+    type: { type: Number, required: true },
     mode: { type: Number, required: true },
-    submode: { type: Number, required: true },
-    minlvl: { type: Number, required: true },
     minplayers: { type: Number, required: true },
     maxplayers: { type: Number, required: true },
   },
 
-  details: Schema.Types.Mixed,
+  flags: {
+    isRstar: { type: Boolean },
+    isRstarVerif: { type: Boolean },
+    list: { type: Schema.Types.Mixed }
+  },
 
-  creator: {
-    nickname: String,
-    medal: { type: Number, required: true },
-    crewId: { type: Number, required: true }
+  // type-specific information
+  specific: {
+    raceP2P: { type: Boolean },
+    raceVehCl: { type: [Number] },
+    raceDefVehicle: { type: Number },
+    raceDistance: { type: Number },
+    raceCheckp: { type: Number },
+    raceLaps: { type: Number },
+    captureType: { type: Number, default: 0 },
+    teamsAmount: { type: Number }
   },
 
   stats: {
-    playedTot: { type: Number, required: true },
-    playedUnq: { type: Number, required: true },
-    quitTot: { type: Number, required: true },
-    quitUnq: { type: Number, required: true },
-    likes: { type: Number, required: true },
-    dislikes: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    ratingReal: { type: Number, required: true }
+    rstar: {
+      playedTot: { type: Number, required: true },
+      playedUnq: { type: Number, required: true },
+      quitTot: { type: Number, required: true },
+      quitUnq: { type: Number, required: true },
+      likes: { type: Number, required: true },
+      dislikes: { type: Number, required: true },
+      bkmk: { type: Number, required: true },
+      rating: { type: Number, required: true },
+      ratingReal: { type: Number, required: true }
+    }
   },
 
   updated: {
-    version: { type: Number, required: true },
+    ver: { type: Number, required: true },
     job: { type: Date, default: Date.now(), required: true },
     info: { type: Date, default: Date.now(), required: true }
   }
 });
 
 jobSchema = new Schema({
-  jobID:    String,
+  jobID: String,
   category: Number,
-  name:     String,
-  desc:     String,
-  image:    String,
+  name: String,
+  desc: String,
+  image: String,
   platform: Number,
 
   info: {
-    mode:       Number,
-    submode:    Number,
-    minlvl:     Number,
+    mode: Number,
+    submode: Number,
+    minlvl: Number,
     minplayers: Number,
     maxplayers: Number,
   },
@@ -70,9 +80,9 @@ jobSchema = new Schema({
 
   creator: {
     nickname: String,
-    medal:    Number,
+    medal: Number,
     crew: {
-      tag:  String,
+      tag: String,
       rank: Number,
       color: {
         type: String,
@@ -82,31 +92,31 @@ jobSchema = new Schema({
   },
 
   ratings: {
-    playedTotal:  Number,
+    playedTotal: Number,
     playedUnique: Number,
-    quitTotal:    Number,
-    quitUnique:   Number,
-    likes:        Number,
-    dislikes:     Number,
-    rating:       Number,
+    quitTotal: Number,
+    quitUnique: Number,
+    likes: Number,
+    dislikes: Number,
+    rating: Number,
   },
 
   updated: {
-    date:    Date,
+    date: Date,
     version: Number
   },
 });
 
 
-jobSchema.virtual('submodeName').get(function() {
+jobSchema.virtual('submodeName').get(function () {
   return config.submodes[this.info.submode];
 });
 
-jobSchema.virtual('platformName').get(function() {
+jobSchema.virtual('platformName').get(function () {
   return config.platforms[this.platform];
 });
 
-jobSchema.methods.getRatingColor = function() {
+jobSchema.methods.getRatingColor = function () {
   let rating = this.ratings.rating;
   return (rating >= 67) ? 'success' : (rating >= 34) ? 'warning' : 'danger';
 };
