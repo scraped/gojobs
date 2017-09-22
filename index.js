dafc45838d4b65d9f4ba9388b4aa1c467cb5e5cb
Andrey Kazakov <tosk-nn@yandex.ru>
2017-09-22 22:24:39 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const config = require('./config');
 const express = require('express');
 const hbars = require('express-handlebars').create(config.handlebars);
+const logger = require('morgan');
 
 const mainRouter = require('./routers/main/main');
 const jobRouter = require('./routers/main/job');
@@ -13,31 +14,44 @@ app.disable('x-powered-by');
 app.set('view engine', '.hbs');
 app.set('port', process.env.PORT || config.port);
 
+//
 // Middleware
+//
+app.use(logger('combined'));
 app.use(express.static('public'));
 
+//
 // Routing
+//
 app.use('/', mainRouter);
 app.use('/job', jobRouter);
 app.use('/admin', adminRouter);
 
+//
 // 404
+//
 app.use((req, res) => {
   console.log(`Error 404 occured at ${req.path}`);
-  res
-    .status(404)
-    .render('error', config.httpErrors.e404);
+  res.status(404).render('error', config.httpErrors.e404);
 });
 
+//
 // Errors
+//
 app.use((err, req, res, next) => {
-  if (typeof err === 'string') return res.send(err);
-  console.error(`Error 500 occured: ${err.stack}`);
-  res
-    .status(500)
-    .render('error', config.httpErrors.e500);
+  console.error(err.stack);
+  res.status(500);
+
+  if (req.xhr) {
+    res.json({ error: err });
+  } else {
+    res.render('error', config.httpErrors.e500);
+  }
 });
 
+//
+// Run server
+//
 app.listen(app.get('port'), () => {
   console.log(`Server is running at http://localhost:${app.get('port')}`);
 });
