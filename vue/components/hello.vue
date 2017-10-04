23d8cc1a24652a4b65ea194ec4c2b053b2f07d18
Andrew Kazakov <tosk-nn@yandex.ru>
2017-10-05 01:16:32 +0300

    Playing with vue here

---
+++ vue/components/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data:  function() {
+    return {
+      message: 'Hello vuorld!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+  h1 {
+    color: blueviolet;
+  }
+</style>
