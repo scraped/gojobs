55d004a95f1828fdc209b19e600b9eff64334158
Andrew Kazakov <tosk-nn@yandex.ru>
2017-10-06 01:26:20 +0300

    .

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Admin from './views/Admin.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/admin', component: Admin }
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router: router
+});
---
+++ vue-cli/src/views/Admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Adminka
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+}
+</script>
+
