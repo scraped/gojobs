004d44a31b2a996205d34b41025bb786b207a106
Andrew Kazakov <tosk-nn@yandex.ru>
2017-10-15 01:28:08 +0300

    .

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import './scss/main.scss'
 
 import Vue from 'vue';
 import App from './App.vue';
-
 import router from './router';
+
+import vueBulmaComponents from 'vue-bulma-components';
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(vueBulmaComponents);
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+});
 
 new Vue({
   el: '#app',
---
+++ src/views/NavMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <navbar>
+    <container>
+      <navbar-brand>
+        <router-link to="/" class="navbar-item">
+          <img src="../images/gtaonline-logo-mini.png" alt="" title="">
+        </router-link>
+
+        <navbar navbar-burger data-target="mainMenu">
+          <span></span><span></span><span></span>
+        </navbar>
+      </navbar-brand>
+
+      <navbar-menu id="mainMenu">
+        <navbar-end>
+          <router-link to="/admin" class="navbar-item">Admin</router-link>
+          <router-link to="/login" class="navbar-item">Log in or sign up</router-link>
+        </navbar-end>
+      </navbar-menu>
+    </container>
+  </navbar>
+</template>
+
+<script>
+import { bulmaComponentGenerator } from 'vue-bulma-components';
+
+export default {
+  name: 'NavMenu',
+  // missing component
+  components: {
+    'navbar-menu': bulmaComponentGenerator('navbar-menu'),
+    'navbar-end': bulmaComponentGenerator('navbar-end'),
+  }
+};
+</script>
+
+<style scoped>
+  .navbar {
+    box-shadow: 0 1px 10px 1px rgba(0, 0, 0, 0.1);
+  }
+</style>
+
