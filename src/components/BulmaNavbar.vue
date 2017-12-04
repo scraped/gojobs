04e7c49b1eaf3d962eafdf6753ae0151ca9699c0
Andrey Kazakov <tosk-nn@yandex.ru>
2017-12-04 21:25:57 +0300

    .

--- src/components/BulmaNavbar.vue
+++ src/components/BulmaNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar is-fixed-top" id="navbar">
+  <nav class="navbar is-fixed-top">
     <div class="container">
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item">
@@ -7,14 +7,14 @@
         </router-link>
         <span class="navbar-item is-size-3" style="font-family: 'SignPainter-HouseScript';">jobs</span>
 
-        <div class="navbar-burger" data-target="mainMenu">
+        <div class="navbar-burger" data-target="navbar-menu">
           <span></span><span></span><span></span>
         </div>
       </div>
 
-      <div class="navbar-menu" id="mainMenu">
+      <div class="navbar-menu" id="navbar-menu">
         <div class="navbar-start">
-           <div class="navbar-item has-dropdown is-hoverable">
+           <div class="navbar-item has-dropdown">
             <a class="navbar-link">
               PC
             </a>
@@ -35,7 +35,7 @@
             <span>Crews</span>
           </router-link>
           <router-link to="/login" class="navbar-item">
-            <span>Log In or Sign Up</span>
+            <span>Sign Up</span>
           </router-link>
         </div>
       </div>
@@ -44,55 +44,66 @@
 </template>
 
 <script>
-// window.addEventListener('DOMContentLoaded', event => {
-//   let navbar = document.getElementById('navbar');
-//   let navbarHeightExtended = navbar.offsetHeight * 3;
-//   let prevScrollPos = window.pageYOffset;
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarBurger = document.querySelector('.navbar-burger');
 
-//   window.addEventListener('scroll', e => {
-//     let currScrollPos = e.pageY;
-//     let hideNavbarPoint = Math.max(navbarHeightExtended, prevScrollPos);
+  navbarBurger.addEventListener('click', () => {
+    const navbarMenuId = navbarBurger.dataset.target;
+    const navbarMenu = document.getElementById(navbarMenuId);
 
-//     if (currScrollPos > hideNavbarPoint) {
-//       navbar.classList.add('navbar-hidden')
-//     } else {
-//       navbar.classList.remove('navbar-hidden')
-//     }
+    navbarBurger.classList.toggle('is-active');
+    navbarMenu.classList.toggle('is-active');
 
-//     if (currScrollPos > navbarHeightExtended) {
-//       navbar.classList.add('navbar-dark')
-//     } else {
-//       navbar.classList.remove('navbar-dark')
-//     }
+  });
 
-//     prevScrollPos = currScrollPos;
-//   });
-// });
+  //
+
+  const navbarDropdown = document.querySelector('.navbar-link');
+
+  document.addEventListener('click', e => {
+    const target = e.target;
+
+    if (target === navbarDropdown) {
+      navbarDropdown.parentElement.classList.toggle('is-active');
+    }
+  })
+
+});
 </script>
+
 
 <style lang="scss">
 @import "../scss/bulma/utilities/_all";
 
 $navbar-height: 4rem;
 $navbar-item-color: $white;
-$navbar-item-hover-color: $danger;
-$navbar-item-hover-background-color: transparent;
+$navbar-item-hover-color: $white;
+$navbar-item-hover-background-color: rgba($black, 0.15);
+
+$navbar-dropdown-arrow: $white;
+$navbar-dropdown-border-top: none;
+$navbar-dropdown-background-color: $dark;
+
+ $navbar-tab-hover-background-color: $dark;
 
 @import "../scss/bulma/components/navbar";
 @import "../scss/vars";
 
 .navbar {
   background-color: transparent;
-  background-image: linear-gradient(to right, $rockstar, $gtaonline);
-  transition-duration: $speed * 2;
+  background-image: linear-gradient(to right, $rockstar, $gtaonline-dark);
 }
 
-.navbar-hidden {
-  transform: translateY(-100px);
+.navbar-item {
+  transition-duration: $speed;
 }
 
-.navbar-dark {
+.navbar-menu.is-active {
   background: $dark;
+}
+
+.navbar-burger {
+  color: $white;
 }
 </style>
 
