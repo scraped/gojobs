9539437a5a7b5920694a9e386e1256b0eee9d12a
Andrew Kazakov <tosk-nn@yandex.ru>
2018-02-13 01:50:10 +0300

    .

--- models/job-details.js
+++ models/job-details.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
   job: { type: Schema.Types.ObjectId, ref: 'Job' },
-  desc: { type: String, required: true, trim: true },
+  desc: { type: String, trim: true, required: true },
   specific: Schema.Types.Mixed
 }, {
   id: false
