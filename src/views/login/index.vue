ee1d0a3b63e2e5a9d634c8cd37bc65285990cf34
Andrew Kazakov <tosk-nn@yandex.ru>
2018-03-18 17:36:46 +0300

    .

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,22 +5,23 @@
         <div class="columns is-centered">
           <div class="column is-half">
             <h1 class="title is-spaced">Sign Up</h1>
-            <h2 class="subtitle is-4">Step 1</h2>
             <b-message>
               <b>Welcome to GTA Online Jobs site.</b><br>
               Процесс регистрации может быть пройден игроками, имеющими действующую учётную запись Rockstar Games Social Club, использующуюся для игры в GTA Online.<br>
-              Для завершения регистрации вам будет предложено опубликовать <b>GTA Online Job</b> с определённым названием <b>в течение 60 минут</b> после начала процесса регистрации, поэтому убедитесь, что:
+              Для завершения регистрации вам будет предложено опубликовать <b>дело GTA Online</b> с определённым названием <b>в течение 60 минут</b> после начала процесса регистрации, поэтому убедитесь, что:
               <ul>
                 <li>Вы введёте имя пользователя, совпадающее с ВАШИМ именем пользователя на сайте Rockstar Games Social Club;</li>
                 <li>Вы будете иметь возможность ЗАЙТИ и ОПУБЛИКОВАТЬ дело в GTA Online в течение следующих 60 минут.</li>
               </ul>
             </b-message>
-            <b-message type="is-warning">
-              <b>Обратите внимание:</b> на данный момент регистрация доступна далеко не всем пользователям: просим извинения за доставленные неудобства.<br>
-              <b>Однако</b>, если она будет доступна - мы ВОЗМОЖНО отправим вам письмо по указанному e-mail.
+            <b-message type="is-danger">
+              <b>Обратите внимание:</b> на данный момент регистрация доступна далеко не всем пользователям. Просим извинения за доставленные неудобства.
             </b-message>
+
             <form method="post" @submit.prevent="signup">
-              <b-field label="YOUR VALID Rockstar Games Social Club Username">
+              <section class="section">
+              <h2 class="subtitle is-4">Step 1</h2>
+              <b-field label="Your valid Rockstar Games Social Club Username *">
                 <b-input
                   size="is-large"
                   v-model="username"
@@ -28,16 +29,34 @@
                 </b-input>
               </b-field>
 
-              <b-field label="E-mail (can be empty)">
-                <b-input
-                  size="is-large"
-                  v-model="email">
-                </b-input>
-              </b-field>
+              <a
+                class="button"
+                href="http://socialclub.rockstargames.com/"
+                target="_blank">Go to RGSC site</a>
+              </section>
 
-              <template v-if="step2">
+              <section class="section" v-if="jobname">
                 <h2 class="subtitle is-4">Step 2</h2>
-                <b-field label="Password" v-if="step2">
+                <b-field label="In order to confirm your identity, please publish a job named:">
+                  <b-input
+                    size="is-large"
+                    v-model="jobname"
+                    required>
+                  </b-input>
+                </b-field>
+                <a
+                  class="button"
+                  href="http://socialclub.rockstargames.com/"
+                  target="_blank">How to publish a job in GTA Online</a><br><br>
+
+                <b-field label="E-mail (optional)">
+                  <b-input
+                    size="is-large"
+                    v-model="email">
+                  </b-input>
+                </b-field>
+
+                <b-field label="Password *">
                   <b-input
                     type="password"
                     size="is-large"
@@ -46,13 +65,12 @@
                   </b-input>
                 </b-field>
 
-
                 <b-field>
                   <b-checkbox v-model="confirm">
                     I confirm that I have published a job named <b>{{ jobname }}</b>.
                   </b-checkbox>
                 </b-field>
-              </template>
+              </section>
 
               <button
                 class="button is-primary"
@@ -76,21 +94,28 @@ export default {
     return {
       password: '',
       email: '',
-      step2: false,
       confirm: false
     }
   },
 
   computed: {
+    username: {
+      get() {
+        return this.$store.state.auth.username;
+      },
+
+      set(value) {
+        this.$store.commit('auth/setUsername', { username: value });
+      }
+    },
     ...mapState('auth', [
-      'username',
       'jobname'
     ])
   },
 
   methods: {
     async signup() {
-      const { username, password, email, step2, confirm } = this;
+      const { username, password, email, confirm } = this;
 
       if (!step2) {
         const response = await axios.post(
@@ -99,8 +124,8 @@ export default {
         );
 
         if (response.status === 200) {
-          this.jobname = response.data.jobname;
-          this.step2 = true;
+          const { jobname } = response.data;
+          this.$store.commit('auth/setJobname', { jobname });
         } else {
           this.$snackbar.open({
             message: response.data.message,
