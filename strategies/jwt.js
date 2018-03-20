70bcb395997c988e969392cb8207c34acde1de06
Andrey Kazakov <tosk-nn@yandex.ru>
2018-03-20 21:31:30 +0300

    .

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -2,13 +2,36 @@ const passport = require('passport');
 const passportJwt = require('passport-jwt');
 const JwtStrategy = passportJwt.Strategy;
 const { ExtractJwt } = passportJwt;
+const User = require('../models/user');
 
 const strategyOptions = {
   secretOrKey: 'supersecretKeyy',
-  jwtFromRequest: ExtractJwt.fromAuthHeader()
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  // passport-jwt is verifying the token using jsonwebtoken. Pass here an
+  // options object for any other option you can pass the jsonwebtoken
+  // verifier. (i.e maxAge)
+  // see https://github.com/auth0/node-jsonwebtoken
+  jsonWebTokenOptions: {
+    maxAge: '7d'
+  }
 };
 
-passport.use(new JwtStrategy(strategyOptions, (payload, done) => {
+passport.use(new JwtStrategy(strategyOptions, async (payload, done) => {
+  const { username } = payload;
 
+  if (!username) {
+    return done(null, false);
   }
-));
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (e) {
+    return done(e);
+  }
+}));
--- strategies/local.js
+++ strategies/local.js
@@ -5,17 +5,15 @@ const User = require('../models/user')
 
 passport.use(new LocalStrategy(async (username, password, done) => {
   const BAD_CREDENTIALS_MESSAGE = 'Username or password is incorrect';
-  let user;
 
   try {
-    user = await User.findOne({ username });
+    const user = await User.findOne({ username });
+
+    if (!user
+      || !user.checkPassword(password)) {
+      return done(null, false, { message: BAD_CREDENTIALS_MESSAGE })
+    }
   } catch (e) {
     return done(e);
   }
-
-  if (!user) {
-    return done(null, false, { message: BAD_CREDENTIALS_MESSAGE })
-  }
-
-  if (user.)
 }));
