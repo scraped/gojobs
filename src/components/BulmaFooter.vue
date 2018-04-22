474100672b4b3d45c0756174978dc6362f91d304
Андрей Казаков <tosk-nn@yandex.ru>
2018-04-23 01:36:28 +0300

    .

--- src/components/BulmaFooter.vue
+++ src/components/BulmaFooter.vue
@@ -2,8 +2,8 @@
   <footer class="footer">
     <div class="container">
       <div class="content">
-        <div class="subtitle has-text-grey">GTA Online Jobs</div>
-        <div><router-link :to="{ path: '/' }">Main page</router-link></div>
+        <div class="subtitle has-text-white-ter">GTA Online Jobs</div>
+        <!-- <div><router-link :to="{ path: '/' }">Main page</router-link></div> -->
       </div>
     </div>
   </footer>
