0a45bf64f36ae8c92e437939f820fefb2bc0f93c
Andrey Kazakov <tosk-nn@yandex.ru>
2018-05-23 21:21:08 +0300

    .

--- strategies/local.js
+++ strategies/local.js
@@ -4,14 +4,11 @@ const LocalStrategy = passportLocal.Strategy;
 const User = require('../models/user')
 
 passport.use(new LocalStrategy(async (username, password, done) => {
-  const BAD_CREDENTIALS_MESSAGE = 'Username or password is incorrect';
-
   try {
     const user = await User.findOne({ username });
 
-    if (!user
-      || !user.checkPassword(password)) {
-      return done(null, false, { message: BAD_CREDENTIALS_MESSAGE })
+    if (!user || !user.checkPassword(password)) {
+      return done(null, false, { message: 'Username or password is incorrect' })
     }
   } catch (e) {
     return done(e);
