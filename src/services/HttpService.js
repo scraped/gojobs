7741a1d97280efaa31c83f72d9dcf87ddd5388cb
Андрей Казаков <tosk-nn@yandex.ru>
2018-07-26 20:04:52 +0300

    .

---
+++ src/services/HttpService.js
@@ -0,0 +1,11 @@
+export default class HttpService {
+  _httpInstance;
+
+  constructor(httpInstance = axiosInstanceFactory.) {
+    this._httpInstance = httpInstance;
+  }
+
+  static axiosInstanceFactory({
+
+  })
+}
