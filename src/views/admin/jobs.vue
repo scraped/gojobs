033d10ad8f4e6fbfcb9a9aabb06c590019549f92
Andrey Kazakov <tosk-nn@yandex.ru>
2018-08-31 19:19:09 +0300

    Admin jobs page - changed fetch form

--- src/views/admin/jobs.vue
+++ src/views/admin/jobs.vue
@@ -6,173 +6,24 @@
           method="post"
           @submit.prevent="fetch"
         >
-          <h2 class="subtitle">Fetch jobs</h2>
-
-          <div class="columns">
-            <div class="column is-half">
-              <b-field label="Type">
-                <b-checkbox v-model="bunches" @input="bunchesCheckboxEvent">
-                  Fetch bunches
-                </b-checkbox>
-              </b-field>
-
-              <b-field label="Author(s)"></b-field>
-              <b-field>
-                <b-radio v-model="category" native-value="all" @input="authorChangeEvent">
-                  {{ categoryAllName }}
-                </b-radio>
-              </b-field>
-
-              <b-field>
-                <b-radio v-model="category" native-value="user" @input="authorChangeEvent">
-                  Specific User
-                </b-radio>
-              </b-field>
-
-              <b-field>
-                <b-radio v-model="category" native-value="crew" @input="authorChangeEvent">
-                  Specific Crew
-                </b-radio>
-              </b-field>
-
-              <b-field>
-                <b-radio v-model="category" native-value="job" @input="authorChangeEvent" :disabled="bunches">
-                  Specific Job
-                </b-radio>
-              </b-field>
-
-              <b-field>
-                <b-radio v-model="category" native-value="rockstar" @input="authorChangeEvent">
-                  Rockstar
-                </b-radio>
-              </b-field>
-
-              <b-field>
-                <b-radio v-model="category" native-value="rockstarverified" @input="authorChangeEvent">
-                  Rockstar Verified
-                </b-radio>
-              </b-field>
-            </div>
-
-            <div class="column">
-              <b-field label="Strict mode">
-                <b-checkbox
-                  v-model="strict"
-                  :disabled="bunches">
-                  Enable strict mode
-                </b-checkbox>
-              </b-field>
-
-              <b-field label="Platform"></b-field>
-
-              <div
-                v-if="platformDisabled"
-                class="field has-text-grey is-italic">
-                Not applicable.
-              </div>
-
-              <b-field v-else>
-                <b-radio-button v-model="platform" native-value="pc">
-                  PC
-                </b-radio-button>
-
-                <b-radio-button v-model="platform" native-value="ps4">
-                  PS4
-                </b-radio-button>
-
-                <b-radio-button v-model="platform" native-value="xboxone">
-                  XB1
-                </b-radio-button>
-              </b-field>
-
-              <b-field
-                label="Username"
-                v-if="category === 'user'">
-                <b-input
-                  v-model.trim="id"
-                  size="is-medium"
-                  placeholder="andreww2012"
-                  required>
-                </b-input>
-              </b-field>
-
-              <b-field
-                label="Crew ID"
-                v-if="category === 'crew'">
-                <b-input
-                  v-model.trim="id"
-                  size="is-medium"
-                  placeholder="Crew digital ID"
-                  required>
-                </b-input>
-              </b-field>
-
-              <template v-if="category === 'job'">
-                <b-field
-                  label="Job ID"
-                  key="permId">
-                  <b-input
-                    v-model.trim="id"
-                    size="is-medium"
-                    minlength="22"
-                    maxlength="22"
-                    required>
-                  </b-input>
-                </b-field>
-
-                <!-- <b-notification
-                  type="is-warning"
-                  :closable="false"
-                  has-icon>
-                  Take into account that sometimes job won't be available by its "permanent" or even "actual" ID: rockstar servers returns either empty object or an empty object with "latest" key set to true.
-                </b-notification> -->
-              </template>
-            </div>
-          </div>
-
-          <!-- <template v-if="bunches">
-            <b-field label="Period"></b-field>
-            <b-field>
-              <b-radio-button v-model="period" native-value="">
-                Any time
-              </b-radio-button>
-
-              <b-radio-button v-model="period" native-value="lastMonth">
-                Last 30 days
-              </b-radio-button>
-
-              <b-radio-button v-model="period" native-value="last7">
-                Last 7 days
-              </b-radio-button>
-
-              <b-radio-button v-model="period" native-value="today">
-                Today
-              </b-radio-button>
-            </b-field>
-          </template> -->
-
-          <b-field
-            v-if="category !== 'job'"
-            label="Requests limit">
+          <h2 class="subtitle">Add job</h2>
+          <b-field grouped>
             <b-input
-              type="number"
-              v-model.number="reqLimit"
+              v-model.trim="jobId"
               size="is-medium"
-              min="1"
-              max="200">
+              placeholder="vyssmboIF0KPxGArqfW9Ww"
+              minlength="22"
+              maxlength="22"
+              :has-counter="false"
+              expanded>
             </b-input>
+            <p class="control">
+              <button class="button is-primary is-medium is-rounded">
+                <b-icon icon="angle-right"></b-icon>
+              </button>
+            </p>
           </b-field>
 
-          <div class="buttons">
-            <button class="button is-primary">Fetch<template v-if="bunches"> bunches</template></button>
-            <a
-              v-if="category === 'job' && id.length === 22"
-              class="button is-light"
-              :href="`https://socialclub.rockstargames.com/games/gtav/jobs/job/${id}`"
-              target="_blank">
-              Proceed to RGSC job page
-            </a>
-          </div>
         </form>
       </div>
     </div>
@@ -212,43 +63,17 @@
 export default {
   data() {
     return {
-      bunches: false,
-      strict: false,
-      category: 'all',
-      id: '',
-      platform: 'pc',
-      period: '',
-      reqLimit: 200,
+      jobId: '',
       processAny: false,
       processLimit: 3000
     }
   },
 
   methods: {
-    bunchesCheckboxEvent(value) {
-      if (value && this.category === 'job') {
-        this.category = 'all';
-      }
-    },
-
-    authorChangeEvent() {
-      this.id = '';
-    },
-
     async fetch() {
-      const {
-        bunches, strict, category, id, platform, period, reqLimit
-      } = this;
+      const {jobId} = this;
 
-      await this.$http.post('/api/job/fetch', {
-        bunches,
-        strict: Number(strict),
-        category,
-        id,
-        platform,
-        period,
-        reqLimit
-      });
+      await this.$http.post('/api/job/fetch', {jobId});
     },
 
     async process() {
@@ -261,19 +86,6 @@ export default {
         any,
         limit
       });
-    }
-  },
-
-  computed: {
-    platformDisabled() {
-      const { category } = this;
-      return category === 'rockstar' || category === 'rockstarverified';
-    },
-
-    categoryAllName() {
-      return this.bunches
-        ? 'RGSC Members'
-        : 'Any jobs';
     }
   }
 };
