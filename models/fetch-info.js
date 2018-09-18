31d5c50cd139b8f0ac092f7913e68c38302b96d5
Андрей Казаков <tosk-nn@yandex.ru>
2018-09-19 01:30:17 +0300

    Better FetchInfo model

--- models/fetch-info.js
+++ models/fetch-info.js
@@ -5,67 +5,81 @@ const {Schema} = mongoose;
 
 function nonRockstar() {
   const {category} = this;
-  return category !== 'rockstar'
-    && category !== 'rockstarverified';
+  return category !== 'rockstar';
 }
 
-const schema = new Schema({
-  type: {
-    type: String,
-    enum: [
-      'rockstar',
-      'rockstarverified',
-      'crew',
-      'user',
-    ],
-    required: true,
-  },
+const schema = new Schema(
+  {
+    type: {
+      type: String,
+      enum: [
+        'rockstar',
+        'crew',
+        'user',
+      ],
+      required: true,
+    },
 
-  id: {
-    type: String,
-    required: nonRockstar,
-  },
+    id: {
+      type: String,
+      validate: {
+        validator(id) {
+          const rockstarId = [
+            'rockstar',
+            'verified',
+          ];
+          return !(this.type === 'rockstar' && !rockstarId.includes(id));
+        },
+        msg: 'If type is "rockstar", id should be either "rockstar" or "verified"',
+      },
+      required: nonRockstar,
+    },
 
-  platform: {
-    type: String,
-    enum: Object.keys(platforms),
-  },
+    plat: {
+      type: String,
+      enum: Object.keys(platforms),
+      required: nonRockstar,
+    },
 
-  total: {
-    type: Number,
-    default: -1,
-    required: true,
-  },
+    total: {
+      type: Number,
+      default: -1,
+      required: true,
+    },
 
-  offset: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
+    offset: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
 
-  firstFetch: {
-    type: Date,
-  },
+    firstFetch: {
+      type: Date,
+    },
 
-  lastFetch: {
-    type: Date,
-  },
+    lastFetch: {
+      type: Date,
+    },
 
-  since: {
-    type: Date,
-  },
+    since: {
+      type: Date,
+    },
 
-  // Needed for pretty complicated fetching manipulations
-  futureSinceDate: {
-    type: Date,
+    // Needed for pretty complicated fetching manipulations
+    futureSinceDate: {
+      type: Date,
+    },
   },
-}, {
-  id: false,
-});
+  {
+    id: false,
+  },
+);
 
+// Define unique compound index
 schema.index(
   {
     type: 1,
+    platform: 1,
     id: 1,
   },
   {unique: true},
